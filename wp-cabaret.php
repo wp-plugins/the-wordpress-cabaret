de1005147153385611ea90b5f357d991960ae5a9
corpodibacco <corpodibacco@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-26 18:41:42 +0000

    git-svn-id: https://plugins.svn.wordpress.org/the-wordpress-cabaret/tags/0.5@98030 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-cabaret.php
+++ wp-cabaret.php
@@ -1,30 +1,11 @@
 <?php
-
 /*
 Plugin Name: The Wordpress Cabaret
 Plugin URI: http://www.italyisfalling.com/wordpress-cabaret/
 Description: Creator of random poetry out of your posts and comments.
-Author: Corpodibacco
+Author: ico
 Author URI: http://www.italyisfalling.com/coding
 Version: 0.5
-*/
-
-
-/*  Copyright 2007  italyisfalling.com  (email : corpodibacco@gmail.com)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 $dir = basename(dirname(__FILE__));
